27583cbb0d3174af5cf495e71ff06355ceb26bfa
Suleyman Farajli <suleyman@farajli.net>
2024-09-05 00:29:56 +0400

    makefile prefix can be set

--- config.mk
+++ config.mk
@@ -4,8 +4,8 @@ VERSION = 1.0
 # customize below to fit your system
 
 # paths
-PREFIX = /usr/local
-MANPREFIX = $(PREFIX)/share/man
+PREFIX ?= /usr/local
+MANPREFIX ?= $(PREFIX)/share/man
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
